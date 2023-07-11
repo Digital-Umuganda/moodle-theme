7766ca2b1cc40e09274e4d6e2453e2fd2d17ebe0
nrep <elvisrugero@gmail.com>
2023-07-11 12:16:41 +0200

    Upgrade version

--- version.php
+++ version.php
@@ -5,7 +5,7 @@
 defined('MOODLE_INTERNAL') || die();                                                                                                
                                                                                                                                     
 // This is the version of the plugin.                                                                                               
-$plugin->version = '2023071016';                                                                                                    
+$plugin->version = '2023071112';                                                                                                    
                                                                                                                                     
 // This is the version of Moodle this plugin requires.                                                                              
 $plugin->requires = '2016070700';                                                                                                   
